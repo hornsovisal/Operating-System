de26da80e5da981b8d075c943a9ca12edf36f155
= <=>
2026-03-10 09:42:24 +0700

    OS

---
+++ W06/mutex_lock.py
@@ -0,0 +1,42 @@
+import threading
+
+
+class Bank:
+    balance = 0
+    lock = threading.Lock()
+
+    def deposit(self):
+        Bank.balance += 100
+
+    def withdraw(self):
+        Bank.balance -= 100
+
+    def get_value(self):
+        return Bank.balance
+
+    def run(self):
+        with Bank.lock:
+            self.deposit()
+            print(f"Value for Thread after deposit {threading.current_thread().name} {self.get_value()}")
+            self.withdraw()
+            print(f"Value for Thread after withdraw {threading.current_thread().name} {self.get_value()}")
+
+
+def main():
+    bank_instance = Bank()
+
+    t1 = threading.Thread(target=bank_instance.run, name="Thread1")
+    t2 = threading.Thread(target=bank_instance.run, name="Thread2")
+    t3 = threading.Thread(target=bank_instance.run, name="Thread3")
+
+    t1.start()
+    t2.start()
+    t3.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+
+
+if __name__ == "__main__":
+    main()
